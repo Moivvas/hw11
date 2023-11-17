e675c291ce07b51cd21d71009a17c18fa8ef0fbb
Moivvas <moivvas@gmail.com>
2023-11-17 22:34:42 +0200

    Confirm email

--- fast_api/src/repository/users.py
+++ fast_api/src/repository/users.py
@@ -22,3 +22,8 @@ async def create_user(body: UserBase, db: Session):
 async def update_token(user: User, refresh_token, db: Session):
     user.refresh_token = refresh_token
     db.commit()
+    
+async def confirmed_email(email: str, db: Session) -> None:
+    user = await get_user_by_email(email, db)
+    user.confirmed = True
+    db.commit()
